a21f6162ff13ae323449e0c56a8aea27407421a2
Jhonatan Restrepo <jhonatan.restrepo@mvm.com.co>
2023-05-29 16:51:18 -0500

    se crean modelos y se registra admin para proyectos

--- proyectos/admin.py
+++ proyectos/admin.py
@@ -1,3 +1,8 @@
+# models
+from .models import Proyecto
+
+# django
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Proyecto)
--- proyectos/models.py
+++ proyectos/models.py
@@ -1,8 +1,11 @@
 # models
 from clientes.models import Cliente
 from direcciones.models import Direccion
+from tecnicos.models import Tecnico
+
 # additional package
 from smart_selects.db_fields import ChainedForeignKey 
+
 # django
 from django.db import models
 from django.core.validators import MinValueValidator
@@ -20,11 +23,12 @@ ESTADOS=(
 
 
 # Create your models here.
-
+min_date = timezone.now().date().strftime('%d-%m-%Y')
+validators = [MinValueValidator(timezone.now().date(), message=f"La fecha debe ser igual o posterior a {min_date}.")]
 class Proyecto(models.Model):
     nombre = models.CharField(blank=False, max_length=100)
     descripcion = models.TextField(blank=True, max_length=500)
-    fecha_fin = models.DateField(validators=[MinValueValidator(timezone.now().date())])
+    fecha_fin = models.DateField(validators=validators)
     fecha_actualizacion =models.DateField(auto_now=True)
     fecha_registro =models.DateField(auto_now_add=True)
     estados = models.CharField(blank=False, null=False, max_length=100, choices=ESTADOS)
@@ -39,3 +43,14 @@ class Proyecto(models.Model):
 
     def __str__(self):
         return self.nombre
+    
+class DetallesProyecto(models.Model):
+    fecha_visita =models.DateField(blank=False, auto_now_add=False, validators=[MinValueValidator(timezone.now().date())])
+    tecnicos = models.ForeignKey(Tecnico, blank=False, on_delete=models.CASCADE)
+    proyecto = models.ForeignKey(Proyecto, blank=False, on_delete=models.CASCADE)
+    fecha_actualizacion= models.DateField(auto_now=True)
+    fecha_registro= models.DateField(auto_now_add=True)
+    descripcion = models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.proyecto
