0cd47f22138350cc7ae4fd8cb986d0f33e4f9a50
Jhonatan Restrepo <jhonatan.restrepo@mvm.com.co>
2023-05-29 16:33:00 -0500

    se crea el modelo y el admin

--- materiales/admin.py
+++ materiales/admin.py
@@ -1,3 +1,18 @@
+# models
+from .models import Material
+
+# django
 from django.contrib import admin
+from django.contrib.humanize.templatetags.humanize import intcomma
+
+
+class MaterialAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'precio_formatted')
+
+    def precio_formatted(self, obj):
+        return intcomma(obj.precio)
+    precio_formatted.short_description = 'Precio'
+
 
 # Register your models here.
+admin.site.register(Material, MaterialAdmin)
--- materiales/models.py
+++ materiales/models.py
@@ -1,3 +1,25 @@
+
+# django
 from django.db import models
+from django.core.exceptions import ValidationError
+
+
+# Create your validations here.
+def validate_valor(value):
+    if not (50 < value < 10_000_000):
+        raise ValidationError("El precio debe ser mayor a 50 y menor a 10.000.000.")
+
 
 # Create your models here.
+class Material(models.Model):
+    nombre = models.CharField(blank=False, max_length=100)
+    precio = models.PositiveIntegerField(validators=[validate_valor])
+    fecha_actualizacion= models.DateField(auto_now=True)
+    fecha_registro = models.DateField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.nombre
+    
+    class Meta:
+        verbose_name = "Material"
+        verbose_name_plural = "Materiales"
