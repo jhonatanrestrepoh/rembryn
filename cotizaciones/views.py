1ebb32177d0bcb5a0a500b5401e27a7eab4df669
Jhonatan Restrepo <jhonatan.restrepo@mvm.com.co>
2023-06-03 19:11:54 -0500

    lista el view y el template para mostrar la cotizacion del un proyecto

--- cotizaciones/views.py
+++ cotizaciones/views.py
@@ -1,7 +1,29 @@
-#django
-from django.shortcuts import render
+# model
+from .models import Cotizacion, DetalleCotizacion
+from proyectos.models import Proyecto
+
+# django
+from django.urls import reverse
+from django.contrib import messages 
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-def proyectos_cotizacion_view(request):
-    pass
+@login_required
+def proyectos_cotizacion_view(request, id):
+    try:
+        cotizacion = Cotizacion.objects.select_related('proyecto').get(proyecto_id=id)
+        detalles_cotizacion = DetalleCotizacion.objects.filter(cotizacion=cotizacion)
+        print(cotizacion.subtotal)
+        for detalle in detalles_cotizacion:
+            print(detalle.materiales.nombre)
+        
+        return render(request, 'cotizaciones/cotizaciones_list.html', {'cotizacion': cotizacion,
+                                                                       'detalles': detalles_cotizacion
+                                                                       })
+    except Cotizacion.DoesNotExist:
+        # redirect a registrar el perfil
+        proyecto = Proyecto.objects.get(id=id)
+        messages.warning(request, f'El proyecto {proyecto.nombre} aún no tiene cotización')
+        url = reverse('proyectos:list')
+        return redirect(url)
--- proyectos/views.py
+++ proyectos/views.py
@@ -1,6 +1,3 @@
-# models
-from .models import Proyecto
-
 # django
 from django.shortcuts import render
 from django.db.models import Max, F
@@ -13,14 +10,12 @@ from django.contrib.auth.decorators import login_required
 def proyectos_list_view(request):
     try:
         cliente = request.user.cliente  # Suponiendo que el modelo de cliente se asocia al usuario mediante un campo OneToOneField
-        # proyectos = cliente.proyecto_set.all()  # Obtener todos los proyectos asociados al cliente
         proyectos = cliente.proyecto_set.annotate(
                 ultima_fecha_visita=Max('visitatecnica__fecha_visita'),
                 nombre_tecnico=F('visitatecnica__tecnico__primer_nombre'),
                 apellido_tecnico=F('visitatecnica__tecnico__primer_apellido')
                 
             ).values('id', 'nombre', 'direccion__direccion', 'ultima_fecha_visita', 'nombre_tecnico', 'apellido_tecnico')
-        print(proyectos)
         return render(request, 'proyectos/proyectos_list.html', {'proyectos': proyectos})
     except Exception as e:
         # redirect a registrar el perfil
