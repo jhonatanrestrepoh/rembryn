5f831aa267a74915e35ce0012aea93ef746b3d0d
Jhonatan Restrepo <jhonatan.restrepo@mvm.com.co>
2023-06-04 23:05:14 -0500

    se configura whitenoise

--- core/settings.py
+++ core/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -183,3 +184,5 @@ LOGGING = {
         },
     },
 }
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
