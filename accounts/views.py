3404be8c98c6d36e2348d02e2ddfb54dfb02f26d
Jhonatan Restrepo <jhonatan.restrepo@mvm.com.co>
2023-06-19 17:23:05 -0500

    en el registrarse que no se borre el user y email

--- accounts/views.py
+++ accounts/views.py
@@ -7,6 +7,7 @@ from django.shortcuts import render, redirect
 from .forms import SignUpForm 
 
 
+
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
@@ -16,5 +17,6 @@ def signup(request):
             url = reverse('login')
             return redirect(url)
     else:
-        form = SignUpForm()
+        initial_data = {'username': request.GET.get('username', ''), 'email': request.GET.get('email', '')}
+        form = SignUpForm(initial=initial_data)
     return render(request, 'registration/signup.html', {'form': form})
