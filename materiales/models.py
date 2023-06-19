9253546a5c259d9672a0091cf55f993450610d7d
Jhonatan Restrepo <jhonatan.restrepo@mvm.com.co>
2023-06-19 15:33:12 -0500

    se cambia para que precio permita dos cifras decimales

--- materiales/models.py
+++ materiales/models.py
@@ -6,14 +6,14 @@ from django.core.exceptions import ValidationError
 
 # Create your validations here.
 def validate_valor(value):
-    if not (50 < value < 10_000_000):
-        raise ValidationError("El precio debe ser mayor a 50 y menor a 10.000.000.")
+    if not (1 <= value < 10_000_000):
+        raise ValidationError("El precio debe ser mayor a 1.00 y menor a 10,000,000.00")
 
 
 # Create your models here.
 class Material(models.Model):
     nombre = models.CharField(blank=False, max_length=100)
-    precio = models.PositiveIntegerField(validators=[validate_valor])
+    precio = models.DecimalField(max_digits=8, decimal_places=2, validators=[validate_valor])
     fecha_actualizacion= models.DateField(auto_now=True)
     fecha_registro = models.DateField(auto_now_add=True)
     
